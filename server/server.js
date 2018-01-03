6ee423efd402ef1e3ab458eafe8516cd6d91f6d5
unknown <kumargaurav9013@gmail.com>
2018-01-03 21:08:51 +0530

    Add GET TODOS Route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,17 @@ app.post('/todos', (req, res) => {
 // });
 
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send('Page not found!');
+    });
+
+});
+
+
 
 
 
