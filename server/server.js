34b7377bde7c5d1e12956f5572cffcd2cce53913
unknown <kumargaurav9013@gmail.com>
2018-01-06 20:03:55 +0530

    DELETE TODO

--- server/server.js
+++ server/server.js
@@ -68,6 +68,30 @@ app.get('/todos/:id', (req, res) => {
 
 
 
+app.delete('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('ID is not Valid');
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+
+        if(!todo){
+            return res.status(404).send('ID Not Found');
+        }
+
+        res.status(200).send({todo});
+
+    }, (e) => {
+        res.status(400).send('ID Not Found0');
+    });
+
+});
+
+
+
 
 
 app.listen(port, () => {
